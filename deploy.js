355189d5a9dbdeed189f4015ac2a26868b2941fd
Kalyan Vishnubhatla <kalvish@gmail.com>
2018-08-11 12:34:43 -0700

    Deploying to rinkeby.

--- deploy.js
+++ deploy.js
@@ -8,5 +8,16 @@ const provider = new HDWalletProvider(
 );
 const web3 = new Web3(provider);
 
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
 
+    console.log('Attempting to deploy from account: ', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: '0x' + bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0] });
+
+    console.log('Contract deployed to: ', result.options.address);
+};
+deploy();
 
